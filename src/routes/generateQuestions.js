9ed1aa83d6116ebd3999a07a01a5380222a391cc
rHirvonen <roope.hirvonen2@gmail.com>
2026-05-21 12:16:15 +0300

    Gemini problem

--- src/routes/generateQuestions.js
+++ src/routes/generateQuestions.js
@@ -16,7 +16,7 @@ router.post("/", async (req, res) => {
     }
 
     const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-flash-latest",
+      model: "gemini-1.5-pro",
     });
 
     const prompt = `
